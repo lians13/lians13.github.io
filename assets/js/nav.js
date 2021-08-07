f56bdc5d2b847a0eba5e1844fd98e43536191bb0
lians13 <88576928+lians13@users.noreply.github.com>
2021-08-07 17:58:14 +0800

    version 0.3
    2021/08/07
    17:57

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -8,14 +8,20 @@ function stick (event) {
     const condition = header.offsetHeight;
 
     if(window.scrollY>= condition){
-        header.style.position = 'fixed';
-        header.style.height = 68+nav.offsetHeight+'px';
+        header.style.position = 'fixed'; 
+        if(nav.style.display == 'flex'){
+            header.style.height = 68+nav.offsetHeight+'px';
+        }else{
+            header.style.height = 68+'px';
+        }
+        header.style.background='rgb(218, 224, 228)';
         header.style.boxShadow = '0px 5px 3px rgba(52,44,52,0.1)';
         banner.style.paddingTop = condition+'px';
         logo.style.width = 68+'px';
     }else{
         header.style.position = 'relative';
-        header.style.height = 88+nav.offsetHeight+'px';
+        header.style.height = 88+'px';
+        header.style.background=' rgba(225,231,235,1)';
         header.style.boxShadow = '0px 0px 0px rgba(52,44,52,0)';
         banner.style.paddingTop = 0;
         logo.style.width = 88+'px'; 
