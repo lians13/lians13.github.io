ac784a4bc9ab8ef62a9b95382caf851cfa24adb4
lians13 <88576928+lians13@users.noreply.github.com>
2021-08-30 21:44:02 +0800

    delet

--- personal_profile/assets/js/scroller.js
+++
@@ -1,6 +0,0 @@
-$('a').click(function(){
-    var href = $(this).attr("href"); 
-    var pos = $(href).offset().top-125;
-    $("html,body").animate({scrollTop: pos}, 800);  
-    return false; 
-});
